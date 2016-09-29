451985229370fab1445ec5c4d3e9d64b0ac460a6
Brian Teng <askmeifyouwantmyusername@gmail.com>
2016-09-29 16:35:10 -0400

    Fixed bug: particle vanishing with projectile

---
+++ Assets/Scripts/ProjectileDespawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileDespawn : MonoBehaviour {
+	private GameObject whoosh;
+	// Use this for initialization
+	void Awake() {
+		whoosh = transform.Find ("particle").gameObject;
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionExit(){
+		whoosh.transform.SetParent (null);
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/ProjectileParticleDespawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileParticleDespawn : MonoBehaviour {
+	private ParticleSystem self;
+	// Use this for initialization
+	void Awake () {
+		self = GetComponent<ParticleSystem> ();
+	}
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.parent == null) {
+			var em = self.emission;
+			em.enabled = false;	
+			if (self.isStopped)
+				Destroy (gameObject);
+		}
+	}
+}
