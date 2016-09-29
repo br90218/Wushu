87ce72dbfa1c165ac5a132df6f9a13a09e23ec62
Brian Teng <askmeifyouwantmyusername@gmail.com>
2016-09-29 00:41:37 -0400

    Basic Landscape / Test Environment / Controls

---
+++ Assets/Scripts/CharacterDetails.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+/// <summary>
+/// Class used to maintain all character details, including health, stamina, and also
+/// passes functions to FPSControllers.
+/// </summary>
+public class CharacterDetails : MonoBehaviour {
+
+	private FirstPersonController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GameObject.FindObjectOfType<FirstPersonController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Climbing(bool enable){
+		controller.EnableClimbing (enable);
+	}
+
+}
---
+++ Assets/Scripts/EnableClimbing.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableClimbing : MonoBehaviour {
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionStay(Collision col) {
+		if (col.gameObject.name == player.name) {
+			Debug.Log ("Hitting FPSController");
+			col.gameObject.GetComponent<CharacterDetails> ().Climbing (true);
+		} 
+	}
+
+	void OnCollisionExit(Collision col) {
+		if (col.gameObject.name == player.name) {
+			col.gameObject.GetComponent<CharacterDetails> ().Climbing (false);
+		}
+	}
+}
